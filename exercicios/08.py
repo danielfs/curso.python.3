29199c27d01a0771ad4c874c2b466003ce4903a5
Daniel Fernandes Silva <danielfs.ti@gmail.com>
2014-09-16 01:15:06 -0300

    Exercício 08.

---
+++ exercicios/08.py
@@ -0,0 +1,20 @@
+print( "Era uma vez, Maria teve uma ovelhinha." )
+print( "Sua lã era branca como a %s." % 'neve' )
+print( "Onde quer que Maria fosse." )
+print( "." * 10 ) # O que isso faz?
+
+fim1 = "Á"
+fim2 = "g"
+fim3 = "u"
+fim4 = "a"
+fim5 = "M"
+fim6 = "i"
+fim7 = "n"
+fim8 = "e"
+fim9 = "r"
+fim10 = "a"
+fim11 = "l"
+
+# Observe o parâmetro end no final do print. O que ele faz?
+print( fim1 + fim2 + fim3 + fim4, end=" " )
+print( fim5 + fim6 + fim7 + fim8 + fim9 + fim10 + fim11 )
