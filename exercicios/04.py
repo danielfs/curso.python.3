91f4322ab337d57ed3b5bd34c32d912b24f7b6c8
Daniel Fernandes Silva <danielfs.ti@gmail.com>
2014-09-16 00:05:06 -0300

    Exercício 04.

---
+++ exercicios/04.py
@@ -0,0 +1,23 @@
+print( "Agora eu vou contar meus frangos:" )
+
+print( "Galinhas", 25 + 30 / 6 )
+print( "Galos", 100 - 25 * 3 % 4 )
+
+print( "Agora eu vou contar meus ovos:" )
+
+print( 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 )
+
+print( "É verdade que 3 + 2 < 5 - 7?" )
+
+print( 3 + 2 < 5 - 7 )
+print( "O que é 3 + 2?", 3 + 2 )
+
+print( "O que é 5 - 7?", 5 - 7 )
+
+print( "Oh, agora eu sei porque é falso." )
+
+print( "Vamos ver mais um pouco." )
+
+print( "É maior?", 5 > -2 )
+print( "É maior ou igual?", 5 >= -2 )
+print( "É menor ou igual?", 5 <= -2 )
