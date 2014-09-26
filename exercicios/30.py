73ae0ce66c89c7b35b546d5d3eaee07e18c0c3a9
Douglas Fernandes Silva <douglasfs.ti@gmail.com>
2014-09-26 11:06:48 -0300

    Exercício 30

---
+++ exercicios/30.py
@@ -0,0 +1,26 @@
+pessoas = 20
+gatos = 30
+cachoros = 15
+
+if pessoas < gatos:
+    print("Muitos gatos! O mundo está condenado")
+
+if pessoas > gatos:
+    print("Poucos gatos! O mundo está salvo.")
+
+if pessoas < cachoros:
+    print("O mundo está todo babado.")
+
+if pessoas > cachoros:
+    print("O mundo está seco.")
+
+cachoros += 5
+
+if pessoas >= cachoros:
+    print("Pessoas é maior ou igual a cachoros")
+
+if pessoas <= cachoros:
+    print("Pessoas é menor ou igual a cachoros")
+
+if pessoas == cachoros:
+    print("Pessoas é igual a cachoros")
