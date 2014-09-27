81bab5a61546a07ab19efba38d35a15ef0188102
Daniel Fernandes Silva <danielfs.ti@gmail.com>
2014-09-27 14:17:43 -0300

    Fazendo o exercício 36.

---
+++ exercicios/my35.py
@@ -0,0 +1,16 @@
+animais = ['pinguim', 'urso', 'tigre', 'zebra']
+
+# segundo elemento
+urso = animais[1]
+
+# todos, menos o último
+todos_menos_ultimo = animais[:-1]
+
+# dois elementos, do final para o início
+animais[-2:]
+
+# do início ao fim, de 2 em 2
+animais[::2]
+
+# do segundo ao fim, de 2 em 2
+animais[1::2]
